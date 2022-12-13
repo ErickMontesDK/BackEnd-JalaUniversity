905696983de4c22b13b5823e0262674b6e58f7dc
ErickDKnight <erickmontes_76@hotmail.com>
2022-12-13 08:09:49 -0600

    fixed bug

--- snake 12-12-2022/src/routes/index.ts
+++ snake 12-12-2022/src/routes/index.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express'
-// import BoardService from '../services/board-services'
 import { AppDataSource } from '../database/db-source'
-// import PositionService from '../services/position-services.ts'
 import { container } from '../inversify/inversify.config'
 import BoardRepository from '../repository/boardRepository'
 import positionRepository from '../repository/positionRepository'
@@ -10,29 +8,30 @@ export const defaultRoute = Router()
 
 defaultRoute.get('/', (req, res) => {
   res.send('Snake game')
-  async function initializeDB () {
-    await AppDataSource.initialize()
-  }
-  initializeDB()
+  // async function initializeDB () {
+  // }
+  // initializeDB()
 })
 defaultRoute.get('/create/:elements', (req, res) => {
   async function createBoard () {
+    await AppDataSource.initialize()
     const number = parseFloat(req.params.elements)
     const boardGenerator = container.get<BoardRepository>('BoardService')
-    // const boardGenerator = new BoardService()
     const newBoard = await boardGenerator.create(number)
     res.send(newBoard)
+    await AppDataSource.destroy()
   }
 
   createBoard()
 })
 defaultRoute.get('/position/:seed', (req, res) => {
   async function getPosition () {
+    await AppDataSource.initialize()
     const number = parseFloat(req.params.seed)
     const positionGenerator = container.get<positionRepository>('PositionService')
-    // const positionGenerator = new PositionService()
     const newPosition = await positionGenerator.create(number)
     res.send(newPosition)
+    await AppDataSource.destroy()
   }
 
   getPosition()
